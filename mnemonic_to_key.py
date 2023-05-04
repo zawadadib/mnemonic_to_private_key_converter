a6e0db824ec2e6be50561b1bde94f8842b0a693e
zawad <110368108+zawadadib@users.noreply.github.com>
2023-05-05 00:53:36 +0600

    Update mnemonic_to_key.py

--- mnemonic_to_key.py
+++ mnemonic_to_key.py
@@ -2,9 +2,9 @@ from eth_account import Account
 
 Account.enable_unaudited_hdwallet_features()
 
-mnemonic = "Enter Your Mnemonic Phrase Here"   # Enter your mnemonic phrase 
-private_key = Account.from_mnemonic(mnemonic)._private_key.hex()                              # Derive the private key from the mnemonic
-public_address = Account.from_key(private_key).address                                        # Derive the public address from the private key
+mnemonic = "Enter Your Mnemonic Phrase Here"                            # Enter your mnemonic phrase 
+private_key = Account.from_mnemonic(mnemonic)._private_key.hex()        # Derive the private key from the mnemonic
+public_address = Account.from_key(private_key).address                  # Derive the public address from the private key
 
 print("Mnemonic to Private key :", private_key)
 print("Your public address :", public_address)
