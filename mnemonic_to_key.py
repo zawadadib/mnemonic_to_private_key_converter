6b38f5d066501d394176384bde491bcc22096167
zawad <110368108+zawadadib@users.noreply.github.com>
2023-05-05 00:33:23 +0600

    Add files via upload
    Initial commit

---
+++ mnemonic_to_key.py
@@ -0,0 +1,10 @@
+from eth_account import Account
+
+Account.enable_unaudited_hdwallet_features()
+
+mnemonic = "mouse person behind matter roast floor dawn educate crew oyster fluid shoulder"   # Enter your mnemonic phrase 
+private_key = Account.from_mnemonic(mnemonic)._private_key.hex()                              # Derive the private key from the mnemonic
+public_address = Account.from_key(private_key).address                                        # Derive the public address from the private key
+
+print("Mnemonic to Private key :", private_key)
+print("Your public address :", public_address)
